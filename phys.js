a05b1a9e1de043c9b9a68724267b0349674fc8c0
k-main <kennkmaina@gmail.com>
2025-03-26 15:09:17 -0700

    multiple objects can now be simulated at once

--- phys.js
+++ phys.js
@@ -60,39 +60,26 @@ document.addEventListener("DOMContentLoaded", () => {
     var inner_dimensions = {"width" : window.innerWidth, "height" : window.innerHeight}
 
     function drawpt(x, y, radx, rady, color, visible=true){
-
         if (visible == false) {
             radx += 1
             rady += 1
             color = 'white'
         }
+        canvas.beginPath()
         canvas.fillStyle = color
         canvas.ellipse(x, cartesian_y(y, canvas_object.height), radx, rady, 0, 0, 2*Math.PI, false)
         canvas.fill()
+        canvas.closePath()
     }
     
-    const c_rad = 10
-    var ratio = 1
-    var c_radx = c_rad
-    var c_rady = c_rad
-
-    if (canvas_object.width > canvas_object.height) {
-        ratio = canvas_object.width / canvas_object.height
-        c_radx = c_radx * ratio
-    } else if (canvas_object.height > canvas_object.width) {
-        ratio = canvas_object.height / canvas_object.width
-        c_rady = c_rady * ratio
-    }
-
-
     var a_y = -9.8
     const dt  = 0.1
     
-    const p = new Point(
+    const p0 = new Point(
         1,
         (canvas_object.width / 2),
         (canvas_object.height / 2),
-        c_rad,
+        10,
         'red',
         20,
         2,
@@ -100,42 +87,90 @@ document.addEventListener("DOMContentLoaded", () => {
         canvas_object.height
     )
     
+    const p1 = new Point(
+        1,
+        (canvas_object.width / 2),
+        (canvas_object.height / 2),
+        5,
+        'blue',
+        50,
+        20,
+        canvas_object.width,
+        canvas_object.height
+    )
 
+    const p2 = new Point(
+        1,
+        (canvas_object.width / 2) + 20,
+        (canvas_object.height / 2) + 20,
+        8,
+        'green',
+        20,
+        80,
+        canvas_object.width,
+        canvas_object.height
+    )
+
+    const p3 = new Point(
+        1,
+        (canvas_object.width / 2) - 20,
+        (canvas_object.height / 2) - 20,
+        3,
+        'red',
+        200,
+        80,
+        canvas_object.width,
+        canvas_object.height
+    )
+
+    points = [p0, p1, p2, p3]
+
+    var current_inner_dimensions = {"width" : window.innerWidth, "height" : window.innerHeight}
     function simulate(){
-        drawpt(p.x, p.y, p.radx, p.rady, p.color, false)
-        var current_inner_dimensions = {"width" : window.innerWidth, "height" : window.innerHeight}
+        canvas.fillStyle = 'white'
+        canvas.fillRect(0, 0, canvas_object.width, canvas_object.height)
+        current_inner_dimensions = {"width" : window.innerWidth, "height" : window.innerHeight}
 
         if (current_inner_dimensions != inner_dimensions) {
             canvas_dm = get_canvas_dm(window.innerWidth, window.innerHeight)
             canvas_object.width = canvas_dm.width;
             canvas_object.height = canvas_dm.height;
             console.log(`New canvas dimensions: ${canvas_dm.width} by ${canvas_dm.height}`)
-            p.update_rad(canvas_object.width, canvas_object.height)
+            for (let i = 0; i < points.length; i++) {
+                points[i].update_rad(canvas_object.width, canvas_object.height)
+            }
         }
-
-        p.x = p.x + p.v_x*dt
-        p.y = p.y + p.v_y*dt
-        if (p.x >= canvas_object.width) {
-            p.v_x = -p.v_x
-            p.x = canvas_object.width - 1
-        }
-        if (p.x <= 0) {
-            p.v_x = -p.v_x
-            p.x = 0
-        }
-
-        if (p.y>= canvas_object.height){
-            p.v_y = -p.v_y
-            p.y = canvas_object.height
-        }
-        if (p.y <= 0){
-            p.v_y = -p.v_y
-            p.y = 0
-        }
-
-        p.v_y = p.v_y + a_y*dt
         
-        drawpt(p.x, p.y, p.radx, p.rady, p.color)
+        for (let i = 0; i < points.length; i++) {
+
+            points[i].x = points[i].x + points[i].v_x*dt
+            points[i].y = points[i].y + points[i].v_y*dt
+            if (points[i].x >= canvas_object.width) {
+                points[i].v_x = -points[i].v_x
+                points[i].x = canvas_object.width - 1
+            }
+            if (points[i].x <= 0) {
+                points[i].v_x = -points[i].v_x
+                points[i].x = 0
+            }
+
+            if (points[i].y>= canvas_object.height){
+                points[i].v_y = -points[i].v_y
+                points[i].y = canvas_object.height
+            }
+            if (points[i].y <= 0){
+                points[i].v_y = -points[i].v_y
+                points[i].y = 0
+            }
+
+            points[i].v_y = points[i].v_y + a_y*dt
+            // drawpt(points[i].x, points[i].y, points[i].radx, points[i].rady, points[i].color)
+        }
+
+        for (let i = 0; i < points.length; i++) {
+            drawpt(points[i].x, points[i].y, points[i].radx, points[i].rady, points[i].color)
+        }
+
         requestAnimationFrame(simulate)
     }
 
