af04a57b98cc9cdc5412dcd6daaec9747f0ceecb
k-main <kennkmaina@gmail.com>
2025-03-25 19:29:51 -0700

    It works, going to make it look nicer now

--- phys.js
+++ phys.js
@@ -14,6 +14,7 @@ function cartesian_y(canvas_y, canvas_height){
 
 document.addEventListener("DOMContentLoaded", () => {
     var canvas_object = document.getElementById("Window")
+    var paused = false
     const canvas = canvas_object.getContext("2d")
     console.log(`Screen inner width: ${window.innerWidth}, inner height: ${window.innerHeight}`)
     console.log(`Canvas width: ${canvas_object.width}, height: ${canvas_object.height}`)
@@ -23,9 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas_object.height = viewport_dm.height;
     console.log(`New canvas width: ${canvas_object.width}, height: ${canvas_object.height}`)
 
-    /* 
-    Scale the viewing window appropriately
-    */
     const c_rad = 10
     var ratio = 1
     var c_radx = c_rad
@@ -39,16 +37,62 @@ document.addEventListener("DOMContentLoaded", () => {
         c_rady = c_rady * ratio
     }
 
-    const c_centerx = (canvas_object.width / 2)
-    const c_centery = (canvas_object.height / 2)
+    var c_x = (canvas_object.width / 2)
+    var c_y = (canvas_object.height / 2)
     const c_rot     = 0
     const c_theta0  = 0
     const c_theta1  = 2 * Math.PI
     const c_drawdir = false
 
     canvas.beginPath()
-    canvas.ellipse(c_centerx, cartesian_y(c_centery, canvas_object.height), c_radx, c_rady, c_rot, c_theta0, c_theta1, c_drawdir)
+    canvas.ellipse(c_x, cartesian_y(c_y, canvas_object.height), c_radx, c_rady, c_rot, c_theta0, c_theta1, c_drawdir)
     canvas.fillStyle = 'red'
     canvas.fill()
 
+    var v_x = 20
+    var v_y = 2
+    var a_y = -9.8
+    var a_x = -1
+    const dt  = 0.1
+    
+    canvas.clearRect(c_x - c_rad, cartesian_y(c_y, canvas_object.height) - c_rad, 2*c_rad, 2*c_rad)
+
+    function simulate(){
+        // canvas.clearRect(c_x - c_rad - 4, cartesian_y(c_y, canvas_object.height) - c_rad - 4, 2*c_rad, 2*c_rad)
+
+        canvas.beginPath()
+        canvas.ellipse(c_x, cartesian_y(c_y, canvas_object.height), c_radx + 1, c_rady + 1, c_rot, c_theta0, c_theta1, c_drawdir)
+        canvas.fillStyle = 'white'
+        canvas.fill()
+
+        c_x = c_x + v_x*dt
+        c_y = c_y + v_y*dt
+        if (c_x >= canvas_object.width) {
+            v_x = -v_x
+            c_x = canvas_object.width - 1
+        }
+        if (c_x <= 0) {
+            v_x = -v_x
+            c_x = 0
+        }
+
+        if (c_y >= canvas_object.height){
+            v_y = -v_y
+            c_y = canvas_object.height
+        }
+        if (c_y <= 0){
+            v_y = -v_y
+            c_y = 0
+        }
+
+        // v_x = ~ no acceleration in the x direction
+        v_y = v_y + a_y*dt
+        canvas.beginPath()
+        canvas.ellipse(c_x, cartesian_y(c_y, canvas_object.height), c_radx, c_rady, c_rot, c_theta0, c_theta1, c_drawdir)
+        canvas.fillStyle = 'red'
+        canvas.fill()
+        requestAnimationFrame(simulate)
+    }
+
+    simulate()
 })
